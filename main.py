e7b794be9ee70bd60488981e6d3f3684a93f01d5
Jmini392 <bread0902@gmail.com>
2025-10-20 20:36:41 +0900

    월드 생성

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from pico2d import *
 import player
 
+# 월드 초기화
+def reset_world():
+    pass
+
+# 월드 업데이트
+def update_world():
+    pass
+
+# 월드 랜더링
+def render_world():
+    pass
+
+
 running = True
 open_canvas()
+reset_world()
 # game loop
 while running:
-    pass
+    update_world()
+    render_world()
+    delay(0.01)
 
 close_canvas()
