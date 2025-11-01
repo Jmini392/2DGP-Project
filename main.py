865fb93b056af77f7ca486e6128ea426d89f783a
Jmini392 <131459220+Jmini392@users.noreply.github.com>
2025-11-01 20:33:46 +0900

    메인 수정

--- main.py
+++ main.py
@@ -1,46 +1,7 @@
 from pico2d import *
+import play_mode
+import framework
 
-import game_world
-from player import Player
-from game_world import *
-
-# 이벤트 처리
-def handle_events():
-    global running
-    event_list = get_events()
-    for event in event_list:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-        else:
-            player.handle_event(event)
-
-# 월드 초기화
-def reset_world():
-    global player
-    player = Player()
-    game_world.add_object(player, 1)
-
-# 월드 업데이트
-def update_world():
-    game_world.update()
-
-# 월드 랜더링
-def render_world():
-    clear_canvas()
-    game_world.render()
-    update_canvas()
-
-
-running = True
 open_canvas()
-reset_world()
-# game loop
-while running:
-    handle_events()
-    update_world()
-    render_world()
-    delay(0.01)
-
+framework.run(play_mode)
 close_canvas()
