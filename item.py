30fc4484a2c2287b979d4089f6ad207108c3bdff
Jmini392 <131459220+Jmini392@users.noreply.github.com>
2025-11-07 21:08:38 +0900

    아이템 이미지 추가

---
+++ item.py
@@ -0,0 +1,20 @@
+from pico2d import *
+
+class Item:
+    def __init__(self):
+        pass
+        self.x, self.y = 400, 300
+        # 아이템 이미지 로드
+        self.image = load_image('sprite/potion.png')
+
+    def draw(self):
+        pass
+        # 아이템 그리기
+        self.image.clip_draw(0, 0, 1800, 1800,self.x, self.y, 50, 50)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+
+    def update(self):
+        pass
