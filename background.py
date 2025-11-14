4883aaa7052ab142d7bde08d875a150aee4358fa
Jmini392 <bread0902@gmail.com>
2025-11-14 12:01:41 +0900

    화면 크기 변경

--- background.py
+++ background.py
@@ -3,12 +3,13 @@ from pico2d import load_image
 background = ['sprite/Fieldground.png', 'sprite/Forestground.png', 'sprite/Battleground.png']
 
 class Background:
-    def __init__(self,stage = 0):
+    def __init__(self,stage = 0, x = 640, y = 360):
+        self.x, self.y = x, y
         self.stage = stage
         self.image = load_image(background[self.stage])
 
     def draw(self):
-        self.image.clip_draw(0, 0, 1920, 1080, 400,300,800,600)
+        self.image.clip_draw(0, 0, 1920, 1080, self.x, self.y, 1280, 720)
 
     def update(self):
         pass
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from pico2d import *
 import play_mode
 import framework
 
-open_canvas()
+open_canvas(1280, 720)
 framework.run(play_mode)
 close_canvas()
