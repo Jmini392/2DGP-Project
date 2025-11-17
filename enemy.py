f5b0084a95f18ef3ac285c31d8014f8850d6220c
Jmini392 <bread0902@gmail.com>
2025-11-17 15:12:46 +0900

    적 이미지 추가후 클래스 수정

--- enemy.py
+++ enemy.py
@@ -4,40 +4,52 @@ import game_world
 import item
 import random
 
-gorgon_image = ['sprite/Gorgon_idle.png']
+gorgon1_image = ['sprite/Gorgon_idle1.png', 'sprite/Gorgon_attack1.png', 'sprite/Gorgon_die1.png']
+gorgon2_image = ['sprite/Gorgon_idle2.png', 'sprite/Gorgon_attack2.png', 'sprite/Gorgon_die2.png']
+gorgon3_image = ['sprite/Gorgon_idle3.png', 'sprite/Gorgon_attack3.png', 'sprite/Gorgon_die3.png']
+gorgon = [gorgon1_image, gorgon2_image, gorgon3_image]
 
 #enemy frame
 TIME_PER_ACTION = 1.0
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
 
-class Enemy:
-    def __init__(self, x = 400, y = 300, name = 'Gorgon', health = 100, attack_power = 10):
+class Gorgon:
+    def __init__(self, x = 400, y = 300, type = 0):
         self.x, self.y = x, y
-        self.name = name
-        self.health = health
-        self.attack_power = attack_power
+        self.health = 100 * (type + 1)
+        self.attack_power = 10 * (type + 1)
+        self.type = type
+        self.attack = False
+        self.die = False
         self.frame = 0
         self.font = load_font('ENCR10B.TTF', 16)
-        if name == 'Gorgon':
-            self.image = load_image(gorgon_image[0])
+        self.idle_image = load_image(gorgon[self.type][0])
+        self.attack_image = load_image(gorgon[self.type][1])
+        self.die_image = load_image(gorgon[self.type][2])
 
     def draw(self):
-        self.image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 128, 128)
+        if self.attack:
+            self.attack_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 128, 128)
+        elif self.die:
+            self.die_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 128, 128)
+        else:
+            self.idle_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 128, 128)
         draw_rectangle(*self.get_bb())
         self.font.draw(self.x - 30, self.y + 50, f'HP: {self.health}', (255, 0, 0))
 
     def get_bb(self):
-        return self.x - 30, self.y - 100, self.x + 50, self.y + 30
+        return self.x - 30, self.y - 70, self.x + 40, self.y + 30
 
     def update(self):
-        self.frame = (self.frame + ACTION_PER_TIME * FRAMES_PER_ACTION * framework.frame_time) % 4
+        self.frame = (self.frame + ACTION_PER_TIME * FRAMES_PER_ACTION * framework.frame_time) % 7
 
     def handle_collision(self, group, other):
         if group == 'attack:enemy':
             # 적 체력 감소
             self.health -= other.damage
             if self.health <= 0:
+                self.die = True
                 game_world.remove_object(self)
                 # 아이템 드랍
                 if (random.random() < 0.5):  # 50% 확률로 아이템 드랍
