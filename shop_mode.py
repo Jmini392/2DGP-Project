80e2a538d0ce339bf870314f66c0c85dd945b04a
Jmini392 <bread0902@gmail.com>
2025-11-18 02:03:59 +0900

    상점 모드 벗어나기 수정

--- shop_mode.py
+++ shop_mode.py
@@ -15,10 +15,9 @@ def finish():
 def handle_events():
     events = get_events()
     for event in events:
-        if event.type == SDL_KEYDOWN and event.type == SDLK_v:
+        if event.type == SDL_KEYDOWN and event.key == SDLK_v:
             framework.pop_mode()
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == 1:
-            # play_mode.player.handle_shop_click(event.x, 600 - event.y)
             pass
 
 def draw():
