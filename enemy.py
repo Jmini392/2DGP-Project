0f499b05b9b5c0dde60d0587dc19edaf65f37f15
Jmini392 <bread0902@gmail.com>
2025-11-24 23:06:41 +0900

    적 플레이어로 이동행동 추가

--- enemy.py
+++ enemy.py
@@ -3,14 +3,17 @@ import framework
 import game_world
 import item
 import random
-from player import Player
+import share
+from behavior_tree import BehaviorTree, Action, Sequence, Condition, Selector
 
-gorgon1_image = ['sprite/Gorgon_idle1.png', 'sprite/Gorgon_attack1.png', 'sprite/Gorgon_die1.png', 'sprite/Gorgon_walk1.png']
-gorgon2_image = ['sprite/Gorgon_idle2.png', 'sprite/Gorgon_attack2.png', 'sprite/Gorgon_die2.png', 'sprite/Gorgon_walk2.png']
-gorgon3_image = ['sprite/Gorgon_idle3.png', 'sprite/Gorgon_attack3.png', 'sprite/Gorgon_die3.png', 'sprite/Gorgon_walk3.png']
-gorgon = [gorgon1_image, gorgon2_image, gorgon3_image]
-wizard1_image = ['sprite/Wizard_idle1.png', 'sprite/Wizard_attack1.png', 'sprite/Wizard_die1.png', 'sprite/Wizard_walk1.png']
-wizard = [wizard1_image]
+animation_names = ['walk', 'idle', 'attack', 'die']
+
+#enemy speed
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 10.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 #enemy frame
 TIME_PER_ACTION = 1.0
@@ -18,53 +21,43 @@ ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
 
 class Gorgon:
+    image = None
+    def load_images(self):
+        if Gorgon.image == None:
+            Gorgon.image = {}
+            for i in range(3):
+                Gorgon.image[i] = {}
+                for name in animation_names:
+                    Gorgon.image[i][name] = load_image("sprite/Gorgon_" + name + str(i + 1) + ".png")
+
     def __init__(self, x = 400, y = 300, type = 0):
         self.x, self.y = x, y
+        self.type = type
+        self.load_images()
+        self.frame = 0
+        self.state = 'idle'
         self.health = 100 * (type + 1)
         self.attack_power = 10 * (type + 1)
-        self.type = type
-        self.attack = False
-        self.die = False
-        self.walk = False
-        self.frame = 0
-        self.face_dir = -1
-        self.font = load_font('ENCR10B.TTF', 16)
-        self.idle_image = load_image(gorgon[self.type][0])
-        self.attack_image = load_image(gorgon[self.type][1])
-        self.die_image = load_image(gorgon[self.type][2])
-        self.walk_image = load_image(gorgon[self.type][3])
+        self.dir = math.atan2(share.player.y - self.y, share.player.x - self.x)
+        self.build_behavior_tree()
 
     def draw(self):
-        if self.face_dir == -1:
-            if self.attack:
-                self.attack_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 128, 128)
-            elif self.die:
-                self.die_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 128, 128)
-            elif self.walk:
-                self.walk_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 128, 128)
-            else:
-                self.idle_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 128, 128)
+        if math.cos(self.dir) < 0:
+            Gorgon.image[self.type][self.state].clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 128, 128)
         else:
-            if self.attack:
-                self.attack_image.clip_draw(int(self.frame) * 128, 0, 128, 128, self.x, self.y, 128, 128)
-            elif self.die:
-                self.die_image.clip_draw(int(self.frame) * 128, 0, 128, 128, self.x, self.y, 128, 128)
-            elif self.walk:
-                self.walk_image.clip_draw(int(self.frame) * 128, 0, 128, 128, self.x, self.y, 128, 128)
-            else:
-                self.idle_image.clip_draw(int(self.frame) * 128, 0, 128, 128, self.x, self.y, 128, 128)
+            Gorgon.image[self.type][self.state].clip_draw(int(self.frame) * 128, 0, 128, 128, self.x, self.y, 128, 128)
         draw_rectangle(*self.get_bb())
 
     def get_bb(self):
         return self.x - 30, self.y - 70, self.x + 40, self.y + 30
 
     def update(self):
-        self.walk = True
-        if self.die:
+        if self.state == 'die':
             div_num = 3
         else:
             div_num = 7
         self.frame = (self.frame + ACTION_PER_TIME * FRAMES_PER_ACTION * framework.frame_time) % div_num
+        self.bt.run()
 
     def handle_collision(self, group, other):
         if group == 'player:enemy':
@@ -82,56 +75,75 @@ class Gorgon:
                     game_world.add_object(dropped_item, 1)
                     game_world.add_collision_pair('player:item', None, dropped_item)
 
+    def is_player_near(self, r = 0.5):
+        if self.distance_less_than(share.player.x, share.player.y, self.x, self.y, r):
+            return BehaviorTree.SUCCESS
+        else:
+            self.state = 'idle'
+            return BehaviorTree.FAIL
+
+    def move_to_player(self, r = 0.5):
+        self.state = 'walk'
+        self.move_little_to(share.player.x, share.player.y)
+        if self.distance_less_than(share.player.x, share.player.y, self.x, self.y, r):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+    def distance_less_than(self, x1, y1, x2, y2, r):
+        distance2 = (x1 - x2) ** 2 + (y1 - y2) ** 2
+        return distance2 < (PIXEL_PER_METER * r) ** 2
+
+    def move_little_to(self, tx, ty):
+        self.dir = math.atan2(ty - self.y, tx - self.x)
+        distance = RUN_SPEED_PPS * framework.frame_time
+        self.x += distance * math.cos(self.dir)
+        self.y += distance * math.sin(self.dir)
+
+    def build_behavior_tree(self):
+        c1 = Condition('Is player near', self.is_player_near, 7)
+        a2 = Action('Move to', self.move_to_player, 2)
+        root = Sequence('Move to target location', c1, a2)
+        self.bt = BehaviorTree(root)
+
 
 class Wizard:
-    def __init__(self, x = 400, y = 300, type = 0):
+    image = None
+    def load_images(self):
+        if Wizard.image == None:
+            Wizard.image = {}
+            for name in animation_names:
+                Wizard.image[name] = load_image("sprite/Wizard_" + name + ".png")
+
+    def __init__(self, x = 400, y = 300):
         self.x, self.y = x, y
+        self.load_images()
+        self.frame = 0
+        self.state = 'idle'
         self.health = 50
         self.attack_power = 20
-        self.type = type
-        self.attack = False
-        self.die = False
-        self.walk = False
-        self.face_dir = -1
-        self.frame = 0
-        self.font = load_font('ENCR10B.TTF', 16)
-        self.idle_image = load_image(wizard[self.type][0])
-        self.attack_image = load_image(wizard[self.type][1])
-        self.die_image = load_image(wizard[self.type][2])
-        self.walk_image = load_image(wizard[self.type][3])
+        self.dir = math.atan2(share.player.y - self.y, share.player.x - self.x)
+        self.build_behavior_tree()
 
     def draw(self):
-        if self.face_dir == -1:
-            if self.attack:
-                self.attack_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 200, 200)
-            elif self.die:
-                self.die_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 200, 200)
-            elif self.walk:
-                self.walk_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 200, 200)
-            else:
-                self.idle_image.clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 200, 200)
+        if math.cos(self.dir) < 0:
+            Wizard.image[self.state].clip_composite_draw(int(self.frame) * 128, 0, 128, 128, 0, 'h', self.x, self.y, 200, 200)
         else:
-            if self.attack:
-                self.attack_image.clip_draw(int(self.frame) * 128, 0, 128, 128, self.x, self.y, 200, 200)
-            elif self.die:
-                self.die_image.clip_draw(int(self.frame) * 128, 0, 128, 128, self.x, self.y, 200, 200)
-            elif self.walk:
-                self.walk_image.clip_draw(int(self.frame) * 128, 0, 128, 128, self.x, self.y, 200, 200)
-            else:
-                self.idle_image.clip_draw(int(self.frame) * 128, 0, 128, 128, self.x, self.y, 200, 200)
+            Wizard.image[self.state].clip_draw(int(self.frame) * 128, 0, 128, 128, self.x, self.y, 200, 200)
         draw_rectangle(*self.get_bb())
 
     def get_bb(self):
         return self.x - 10, self.y - 100, self.x + 50, self.y + 10
 
     def update(self):
-        if self.die or self.walk:
+        if self.state == 'die' or self.state == 'walk':
             div_num = 6
-        elif self.attack:
+        elif self.state == 'attack':
             div_num = 8
         else:
             div_num = 7
         self.frame = (self.frame + ACTION_PER_TIME * FRAMES_PER_ACTION * framework.frame_time) % div_num
+        self.bt.run()
 
     def handle_collision(self, group, other):
         if group == 'attack:enemy':
@@ -145,3 +157,34 @@ class Wizard:
                     dropped_item = item.Item(self.x + 10, self.y - 20, random.randint(0, 2))
                     game_world.add_object(dropped_item, 1)
                     game_world.add_collision_pair('player:item', None, dropped_item)
+
+    def is_player_near(self, r=0.5):
+        if self.distance_less_than(share.player.x, share.player.y, self.x, self.y, r):
+            return BehaviorTree.SUCCESS
+        else:
+            self.state = 'idle'
+            return BehaviorTree.FAIL
+
+    def move_to_player(self, r=0.5):
+        self.state = 'walk'
+        self.move_little_to(share.player.x, share.player.y)
+        if self.distance_less_than(share.player.x, share.player.y, self.x, self.y, r):
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+    def distance_less_than(self, x1, y1, x2, y2, r):
+        distance2 = (x1 - x2) ** 2 + (y1 - y2) ** 2
+        return distance2 < (PIXEL_PER_METER * r) ** 2
+
+    def move_little_to(self, tx, ty):
+        self.dir = math.atan2(ty - self.y, tx - self.x)
+        distance = RUN_SPEED_PPS * framework.frame_time
+        self.x += distance * math.cos(self.dir)
+        self.y += distance * math.sin(self.dir)
+
+    def build_behavior_tree(self):
+        c1 = Condition('Is player near', self.is_player_near, 7)
+        a2 = Action('Move to', self.move_to_player)
+        root = Sequence('Move to target location', c1, a2)
+        self.bt = BehaviorTree(root)
